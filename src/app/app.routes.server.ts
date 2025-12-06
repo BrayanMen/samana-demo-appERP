1295580e62646e62ee2c1cf98f7c0201b2c713e8
BrayanMen <brayanjmr880@gmail.com>
2025-12-06 14:50:35 -0300

    first commit2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,40 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'employees/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'employees/:id/assign-tools',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'works/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'works/:id/team',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'works/:id/assign-tools',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'inventory/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'finances/obra/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'incidents/:id',
+    renderMode: RenderMode.Client,
+  },
+
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
